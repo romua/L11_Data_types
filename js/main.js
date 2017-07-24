c19c8442556ece76938385d240c09f4de23d77b4
leap <romafromukraine@gmail.com>
2017-07-24 12:29:53 +0300

    Added two last functions. Not tested

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ function forEach(arr, callback){
 		callback(arr[i]);
 	}
 }
+function transform(el){return el*el}
 
 function getTransformedArray(arr, callback){
 	var arrOfTransformedItems = [];
@@ -19,7 +20,9 @@ function pluckByAttribute(arrObj, label) {
 	getTransformedArray(arrObj, function(el){
 		result.push(el[label]);
 	});
+	console.log(result);
 	return result;
+
 }
 
 function cypherPhrase(obj, string){
@@ -32,8 +35,9 @@ function cypherPhrase(obj, string){
 		outputMessage.push(obj[el] || inputMessage[i]);
 		i++;	
 	});
-	
+	console.log(outputMessage);
 	return outputMessage.join('');
+
 }
 
 function decypherPhrase(obj, string){
@@ -57,6 +61,33 @@ function decypherPhrase(obj, string){
 }
 
 
+function getTopNRichestNames(number, arr){
+	var dollars = {B: 1e9, M: 1e6, K: 1e3};
+	var resultArrObj = [];
+	var i = 0;
+	var someArr = pluckByAttribute(arr, 'income');
+	var arrOfNames = pluckByAttribute(arr, 'name' );
+	console.log(someArr);
+	getTransformedArray(someArr, function(el){
+		var incomeChar = someArr[i].slice(someArr[i].length-1);
+		console.log(incomeChar);
+		var incomeNum = someArr[i].slice(0, someArr[i].length-1);
+		console.log(incomeNum);
+		console.log(incomeNum*dollars[incomeChar]);
+		resultArrObj.push({ name: arrOfNames[i], income: incomeNum*dollars[incomeChar]});
+		i++;	
+	});
+	resultArrObj.sort(function (a, b) {
+		return b.income - a.income;
+	});
+	console.log(resultArrObj.slice(0, number));
+}
 
-var charactersMap = {a: 'o', c: 'd', t: 'g'}
- //cypherPhrase( charactersMap, 'kiggy dog' );
+var charactersMap = {a: 'o', c: 'd', t: 'g'};
+var people = [
+	{name: 'Bara', income: '1B'},
+	{name: 'Dara', income: '500B'},
+	{name: 'Kara', income: '1M'},
+	{name: 'Zara', income: '2K'}
+];
+
